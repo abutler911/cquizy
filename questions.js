2083903b9a24c0005c46348796952f144ced6a2e
abutler911 <abutler911@gmail.com>
2025-01-21 15:14:26 -0700

    added questions

--- questions.js
+++ questions.js
@@ -8,11 +8,14 @@ const questions = [
   },
   {
     category: "Pre-Departure Ground Ops",
+    context: "Cold dark flight deck, battery 1 selected on",
     question: "What is the procedure is the battery voltage is below 22.5V?",
     answer: "Use the recharge procedure in the SOPM.",
   },
   {
     category: "Pre-Departure Ground Ops",
+    context:
+      "AC Electrical power is established, safety and power-up checklist is complete.",
     question:
       "The E-PBIT is automatically performed when an initial AC source is introduced. What actions will interrupt this test?",
     answer:
@@ -20,6 +23,8 @@ const questions = [
   },
   {
     category: "Pre-Departure Ground Ops",
+    context:
+      "AC Electrical power is established, safety and power-up checklist is complete.",
     question:
       "If AC power is lost, what is the procedure for re-establishing AC power?",
     answer:
@@ -27,23 +32,28 @@ const questions = [
   },
   {
     category: "Pre-Departure Ground Ops",
+    context:
+      "AC Electrical power is established, safety and power-up checklist is complete.",
     question:
       "BATT 1 OVERTEMP is displayed on the EICAS. What is the procedure?",
     answer: "Memory item, QRC",
   },
   {
     category: "Pre-Departure Ground Ops",
+    context: "Originating and Before Start",
     question:
       "What is the minimum hydraulic reservoir temperature for engine start?",
     answer: "-18C",
   },
   {
     category: "Pre-Departure Ground Ops",
+    context: "Originating and Before Start",
     question: "What is the maximum fuel capacity?",
     answer: "20,XXX lbs",
   },
   {
     category: "Pre-Departure Ground Ops",
+    context: "Originating and Before Start",
     question:
       "During fueling ops, a FUEL IMBALANCE caution message is presented on the EICAS, what is the maximum fuel imbalance?",
     answer: "794 lbs",
