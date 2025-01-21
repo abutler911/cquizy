20e7dd6d4d22e0e5fce1b199b5b7e5018616ec81
abutler911 <abutler911@gmail.com>
2025-01-21 12:56:42 -0700

    adding questions

--- questions.js
+++ questions.js
@@ -1,28 +1,33 @@
 const questions = [
   {
-    category: "General",
-    question: "What is the maximum altitude for RVSM airspace?",
-    answer: "41,000 feet",
-  },
-  {
-    category: "Procedures",
-    question: "What is the standard takeoff briefing flow?",
-    answer: "Weather, runway, SID, threats, and actions",
-  },
-  {
-    category: "Emergencies",
-    question: "What is the memory item for an engine fire?",
-    answer: "Throttle - Idle, Fuel cutoff, Fire handle - Pull",
-  },
-  {
-    category: "Performance",
-    question: "What is the definition of V1?",
+    category: "Pre-Departure Ground Ops",
+    question: "What is the minimum voltage for batteries 1 & 2?",
     answer:
-      "The speed beyond which takeoff must continue in the event of an engine failure",
+      "Above 22.5 or if between 21.0 and 22.5 with the recharge procedure",
   },
   {
-    category: "Systems",
-    question: "How many hydraulic systems does the aircraft have?",
-    answer: "Three",
+    category: "Pre-Departure Ground Ops",
+    question: "What is the procedure is the battery voltage is below 22.5V?",
+    answer: "Use the recharge procedure in the SOPM.",
+  },
+  {
+    category: "Pre-Departure Ground Ops",
+    question:
+      "The E-PBIT is automatically performed when an initial AC source is introduced. What actions will interrupt this test?",
+    answer:
+      "Interruption of AC power, pressurizing any hydraulic system, activated any of the FCM buttons.",
+  },
+  {
+    category: "Pre-Departure Ground Ops",
+    question:
+      "If AC power is lost, what is the procedure for re-establishing AC power?",
+    answer:
+      "Power down using leaving the airplane checklist and then power back up using the safety and power-up checklist.",
+  },
+  {
+    category: "Pre-Departure Ground Ops",
+    question:
+      "BATT 1 OVERTEMP is displayed on the EICAS. What is the procedure?",
+    answer: "Memory item, QRC",
   },
 ];
